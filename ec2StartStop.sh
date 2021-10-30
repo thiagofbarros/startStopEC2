7791e48d5eae1e73ec96fd0b6ccd52fda464d66b
Thiago Barros <thiagofbarros@outlook.com>
2021-10-30 18:27:06 -0300

    Start/Stop EC2 instances

--- ec2StartStop.sh
+++ ec2StartStop.sh
@@ -23,38 +23,44 @@ options() {
 	publicip 	Show Public IP"
 }
 
-if [ $# -ne 2 -o $# = 0 ]; then
-	options
-  	exit 2
-fi
-
-EC2INSTANCE=$2
 REGION=us-east-1
+EC2INSTANCE=$2
 EC2STATE=$(getEC2Status)
 
-if [ "$1" = "start" ]; then
-	if [ "$EC2STATE" = "running"  ]; then
-		echo "Instance already running"
-	else
-		echo "Starting instance $EC2INSTANCE..."
-		startEC2
+case "$1" in
+	"start")
+		if [ "$EC2STATE" = "running"  ]; then
+			echo "Instance already running"
+		else
+			echo "Starting instance $EC2INSTANCE..."
+			startEC2
+			exit $?
+		fi
+	;;	
+ 	"stop")
+		if [ "$EC2STATE" = "stopped"  ]; then
+			echo "Instance already stopped"
+			exit $?
+		else
+			echo "Stopping instance $EC2INSTANCE..."
+			stopEC2
+		fi
+	;;	
+	"status")
+		echo "Displaying state of $EC2INSTANCE"
+		getEC2Status
 		exit $?
-	fi
-elif [ "$1" = "stop" ]; then
-	if [ "$EC2STATE" = "stopped"  ]; then
-        echo "Instance already stopped"
+	;;	
+ 	"publicip")
+		echo "Public IP: $(showPublicIP)"
 		exit $?
-    else
-		echo "Stopping instance $EC2INSTANCE..."
-        stopEC2
-    fi
-elif [ "$1" = "status" ]; then
-	echo "Displaying state of $EC2INSTANCE"
-	getEC2Status
-	exit $?
-elif [ "$1" = "publicip" ]; then
-	echo "Public IP: $(showPublicIP)"
-	exit $?
-fi
+	;;
+	"help")
+		options
+	;;
+	*)
+		options
+	;;	
+esac
 
 exit $?
